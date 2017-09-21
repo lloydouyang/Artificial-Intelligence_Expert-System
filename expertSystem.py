1198c0e590c9a9e4b0ca071616e35afef8bf41a0
lloydouyang <do5xb@virginia.edu>
2017-09-20 22:57:35 -0400

    Test Evaluation of Expressions in Learn

--- expertSystem.py
+++ expertSystem.py
@@ -1,5 +1,5 @@
 # Expert System
-# Monica Kuo (mdk6jd); Ouyang De (do5xb)
+# Monica Kuo (mdk6jd); De Ouyang (do5xb)
 
 import textwrap
 
@@ -12,113 +12,183 @@ rules = []
 
 def expertSystem():
 
-	while(True):
+    while(True):
 
-		# get input 
-		commandString = input()
-		# parse input
-		commandList = commandString.split(' ')
-		# call correct function
-		if(commandList[0] == "Teach"):
-			if((commandList[1] == "-R") or (commandList[1] == "-L")):
-				teachVariableDefinition(commandList)
-			if(commandList[1] in variableDefinitions.keys() and (commandList[3] == "true" or commandList[3] == "false")):
-				teachTruth(commandList)
-			if((commandList[2]) == "->"):
-				teachNewRule(commandList)
+        # get input
+        commandString = input()
+        # parse input
+        commandList = commandString.split(' ')
+        # call correct function
+        if(commandList[0] == "Teach"):
+            if((commandList[1] == "-R") or (commandList[1] == "-L")):
+                teachVariableDefinition(commandList)
+            if(commandList[1] in variableDefinitions.keys() and (commandList[3] == "true" or commandList[3] == "false")):
+                teachTruth(commandList)
+            if((commandList[2]) == "->"):
+                teachNewRule(commandList)
 
-		if(commandList[0] == "List"):
-			listCommand()
+        if(commandList[0] == "List"):
+            listCommand()
 
-		if(commandList[0] == "Learn"):
-			learnCommand()
+        if(commandList[0] == "Learn"):
+            learnCommand()
 
-		if(commandList[0] == "Query"):
-			queryCommand()
+        if(commandList[0] == "Query"):
+            queryCommand()
 
-		if(commandList[0] == "Why"):
-			whyCommand()
+        if(commandList[0] == "Why"):
+            whyCommand()
 
-		# print(commandList)
-		
+        # print(commandList)
+
 # Teach <ARG> <VAR> = <STRING>
 def teachVariableDefinition(commandList):
-	# get information
-	argument = commandList[1]
-	variable = commandList[2]
-	value = ' '.join(commandList[4::])
-	# add information to variableDefinitions
-	variableDefinitions[variable] = [value, argument, "false"]
+    # get information
+    argument = commandList[1]
+    variable = commandList[2]
+    value = ' '.join(commandList[4::])
+    # add information to variableDefinitions
+    variableDefinitions[variable] = [value, argument, "false"]
 
 # Teach <ROOT VAR> = <BOOL>
 def teachTruth(commandList):
-	# get information
-	variable = commandList[1]
-	truth = commandList[3]
-	typeOfVariable = variableDefinitions[variable][1]
-	# this command can only be used with root variables
-	if(typeOfVariable == "-R"):
-		facts[variable] = truth
-		variableDefinitions[variable][2] = truth
-		# reset all learned variables to the value of false
-		for variable, information in variableDefinitions.items():
-			if(information[1] == "-L"):
-				information[2] = 'false'
-				facts[variable] = 'false'
+    # get information
+    variable = commandList[1]
+    truth = commandList[3]
+    typeOfVariable = variableDefinitions[variable][1]
+    # this command can only be used with root variables
+    if(typeOfVariable == "-R"):
+        facts[variable] = truth
+        variableDefinitions[variable][2] = truth
+        # reset all learned variables to the value of false
+        for variable, information in variableDefinitions.items():
+            if(information[1] == "-L"):
+                information[2] = "false"
+                facts[variable] = "false"
 
-	else:
-		print("Error: Cannot set a learned variable directly")
+    else:
+        print("Error: Cannot set a learned variable directly")
 
 # Teach <EXP> -> <VAR>
 def teachNewRule(commandList):
-	# check to make sure that all variables here have already been defined via previous Teach commands
-	for letter in commandList[1]:
-		# ignore command if variables in <EXP> are not defined 
-		if( (letter not in "!&|()") and letter not in variableDefinitions.keys() ):
-			return
-	# ignore command if variable <VAR> is not defined
-	if( commandList[3] not in variableDefinitions.keys() ):
-		return 
+    # check to make sure that all variables here have already been defined via previous Teach commands
+    for letter in commandList[1]:
+        # ignore command if variables in <EXP> are not defined
+        if( (letter not in "!&|()") and letter not in variableDefinitions.keys() ):
+            return
+    # ignore command if variable <VAR> is not defined
+    if( commandList[3] not in variableDefinitions.keys() ):
+        return
 
-	# add to rules 
-	rules.append(' '.join(commandList[1:]))
+    # add to rules
+    rules.append(' '.join(commandList[1:]))
 
 def listCommand():
 
-	print("Root Variables:")
-	for variable, information in variableDefinitions.items():
-		if(information[1] == "-R"):
-			print(' '*5 + variable + ' = ' + information[0])
-	print()
+    print("Root Variables:")
+    for variable, information in variableDefinitions.items():
+        if(information[1] == "-R"):
+            print(' '*5 + variable + ' = ' + information[0])
+    print()
 
-	print("Learned Variables:")
-	for variable, information in variableDefinitions.items():
-		if(information[1] == "-L"):
-			print(' '*5 + variable + ' = ' + information[0])
-	print()
+    print("Learned Variables:")
+    for variable, information in variableDefinitions.items():
+        if(information[1] == "-L"):
+            print(' '*5 + variable + ' = ' + information[0])
+    print()
 
-	print("Facts:")
-	for variable, fact in facts.items():
-		if(fact == "true"):
-			print(' '*5 + variable)
-	print()
+    print("Facts:")
+    for variable, fact in facts.items():
+        if(fact == "true"):
+            print(' '*5 + variable)
+    print()
 
-	print("Rules:")
-	for rule in rules:
-		print(" "*5 + rule)
-	print()
+    print("Rules:")
+    for rule in rules:
+        print(" "*5 + rule)
+    print()
+# To evaluate expression to give true or false, @ means true, # means false
+def evaluate(s):
 
+    # get rid of ()
+    q=s.find("(")
+    qq=s.find(")")
+    while (q!=-1):
+        flag2= evaluate(s[q+1:qq])
+        if flag2:
+            s.replace(s[q:qq+1],"@")
+        else:
+            s.replace(s[q:qq+1],"#")
+    p=s.find("!")
+    while p!=-1:
+        for i in xrange(p+1,len(s),1):
+            if (s[i] in "!&|()"):
+                i=i-1
+                break
+        subs=s[p:i]
+        if facts[subs]=="false" or subs=="#":
+            s.replace(s[p:i+1],"@")
+        else:
+            s.replace(s[p:i+1],"#")
+        p=s.find("!")
+    # get rid of &
+    p=s.find("&")
+    while (p!=-1):
+        for i in xrange(p+1,len(s),1):
+            if (s[i] in "!&|()"):
+                i=i-1
+                break
+        for j in xrange(p-1,-1,-1):
+            if (s[j] in "!&|()"):
+                j=j+1
+                break
+        subs=s[p:i]
+        subs2=s[j:p]
+        if (facts[subs]=="true" or subs=="@" ) and (facts[subs2]=="true" or subs2=="@" ):
+            s.replace(s[j:i+1],"@")
+        else:
+            s.replace(s[j:i+1],"#")
+        p=s.find("&")
+    # get rid of |
+    p=s.find("|")
+    while (p!=-1):
+        for i in xrange(p+1,len(s),1):
+            print(p+1)
+            print(len(s))
+            if (s[i] in "!&|()"):
+                i=i-1
+                break
+        for j in xrange(p-1,-1,-1):
+            if (s[j] in "!&|()"):
+                j=j+1
+                break
+        subs=s[p:i]
+        subs2=s[j:p]
+        if facts[subs]=="true" or subs=="@" or facts[subs2]=="true" or subs2=="@":
+            s.replace(s[j:i+1],"@")
+        else:
+            s.replace(s[j:i+1],"#")
+        p=s.find("|")
+    if s=="@":
+        return true
+    else:
+        return false
 
 def learnCommand():
-	print("in learnCommand")
+
+
+
+    print("in learnCommand")
+    print(evaluate("S&V"))
+    #print(evaluate("S|V&!(V|S)"))
 
 # Query <EXP>
 def queryCommand():
-	print("in queryCommand")
+    print("in queryCommand")
 
 # Why <EXP>
 def whyCommand():
-	print("in whyCommand")
+    print("in whyCommand")
 
 if __name__ == "__main__":
-	expertSystem()
+    expertSystem()
